d443c930b7c76b854d6ca3a6abe0dc8be1fc81c5
Helen-Acer <josue.contreras21@hotmail.com>
2018-08-01 23:54:00 -0400

    initial commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h1>Calculator component goes here...</h1>;
+  }
+}
+
+export default Calculator;
