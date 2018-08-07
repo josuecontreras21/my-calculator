9a55443a00c2c495c19afa08b991947447c0edbf
Josué Contreras <josue.contreras21@gmail.com>
2018-08-07 00:53:46 -0400

    add h1 class to display

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,7 +3,7 @@ import React from "react";
 const Display = props => {
   let { display } = props;
   return (
-    <div className="display float-right" id="display">
+    <div className="display float-right h1" id="display">
       {display}
     </div>
   );
